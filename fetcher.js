d91d0f9a72b1b65c0161a1190e00760ac89a91ba
Rora Alem <rora.goitom@gmail.com>
2023-06-20 17:06:30 -0400

    Change function expression style

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,4 @@
-// Page Downloader 
+// Page Downloader
 // use previous knowledge of working with http & file systems
 /* Implementation of node app - fetcher:-
 
@@ -13,12 +13,14 @@
   Async operations:
     - make http request & wait for the response
     - after request, take data received & write it to a file in path
-    - use nested callbacks to control asynchronous operations 
+    - use nested callbacks to control asynchronous operations
  */
 
 //require request and fs
 const request = require('request');
 const fs = require('fs');
+const { constants } = fs;
+
 
 //retrieve command line arguments
 const url = process.argv[2];
@@ -32,10 +34,10 @@ if (!url || !filePath) {
 
 //check if the url is valid
 
-function isUrlValid(url) {
+const isUrlValid = function(url) {
   const regex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
   return regex.test(url);
-}
+};
 
 // usage
 
@@ -46,38 +48,39 @@ if (!isUrlValid(url)) {
 
 
 // check if the file path is valid
-function isPathValid(filePath) {
-    try {
-        fs.access(filePath);
-        return true;
-      } catch (error) {
-        return false;
-        }
-}
+const isPathValid = function(filePath) {
+  try {
+    fs.accessSync(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
     
 // usage
 if (!isPathValid(filePath)) {
-    console.error('Invalid file path');
-    process.exit(1);
+  console.error('Invalid file path');
+  process.exit(1);
 }
+
 //use nested callbacks to control the asynchronous operations
 // make http request
 request(url, (error, response, body) => {
   // Print the error if one occurred during the HTTP request
-  if(error){
+  if (error) {
     console.error('Error:', error);
     process.exit(1);
   }
-   
- fs.writeFile(filePath, body,(error) =>{
+ 
+  fs.writeFile(filePath, body,(error) =>{
     //print if any errors during file writing
-    if (error){
+    if (error) {
       console.error("Failed to write to specified local path");
       process.exit(1);
     } else {
       //If successful, print this message with length of the downloaded data and file path
-        console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
-      }
-  }); 
+      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+    }
+  });
 });
 
